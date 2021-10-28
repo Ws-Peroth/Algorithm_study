c003b56c8b49c1e3c8c2f50b8fa6cdc31958baee
Ws-Peroth <a01033056809@gmail.com>
2021-10-28 10:51:51 +0900

    Solve

--- Algorithm_Python/Study_04/que_03.py
+++ Algorithm_Python/Study_04/que_03.py
@@ -1,28 +1,30 @@
-# 푸는 중
+# 등굣길
 # https://programmers.co.kr/learn/courses/30/lessons/42898
-def solution(m, n, puddles):
-    factorial = [1 for i in range(m + n + 1)]
-    
-    for i in range(2, len(factorial)):
-        factorial[i] = i * factorial[ i - 1 ]
 
-    answer = factorial[m + n - 2] // (factorial[m - 1] * factorial[n - 1])
+def solution(m, n, puddles):
+    dp = [[1 for i in range(m)] for j in range(n)]
     
     for i in range(len(puddles)):
-        x, y = puddles[i][0], puddles[i][1]
-        
-        answer -= (factorial[x + y - 2] // (factorial[x - 1] * factorial[y - 1])) * (factorial[ (m - x) + (n - y)]) // (factorial[m - x] * factorial[n - y])    
-    return int(answer % 1000000007)
+        x = puddles[i][0] - 1
+        y = puddles[i][1] - 1
+        dp[y][x] = 0
 
+    for x in range(1, m):
+        if(dp[0][x] != 0):
+            dp[0][x] = dp[0][x - 1]
+            
+    for y in range(1, n):
+        if(dp[y][0] != 0):
+            dp[y][0] = dp[y - 1][0]
+    
+    for y in range(1, n):
+        for x in range(1, m):
+            targetValue = dp[y][x]
+            
+            if(targetValue != 0):
+                dp[y][x] = dp[y - 1][x] + dp[y][x - 1]
+                
+    return dp[y][x] % 1000000007
 
 print(solution(6, 5, [[3, 3]]))
 
-
-factorial = [1 for i in range(10)]
-
-for i in range(2, len(factorial)):
-    factorial[i] = i * factorial[ i - 1 ]
-
-
-
-print(factorial[6] / (factorial[3]**2))
