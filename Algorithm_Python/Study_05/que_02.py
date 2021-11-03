064ff779f938170f94e27a16735c9b28b7f7f271
Ws-Peroth <a01033056809@gmail.com>
2021-11-03 20:27:37 +0900

    Solve Fix

--- Algorithm_Python/Study_05/que_02.py
+++ Algorithm_Python/Study_05/que_02.py
@@ -2,15 +2,18 @@
 #https://programmers.co.kr/learn/courses/30/lessons/12913
 
 def solution(dp):
+    answer = 0
+
     for i in range(1, len(dp)):
         for j in range(4):
-            target = dp[i][j]
+            temp = dp[i][j]
             for k in range(4):
                 if(j == k):
                     continue
                 else:
-                    dp[i][j] = max(dp[i][j], target + dp[i - 1][k])
+                    dp[i][j] = max(dp[i][j],temp + dp[i - 1][k])
 
+    
     return max(dp[len(dp) - 1])
 
 print(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]))
